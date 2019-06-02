ec83a19175de4aa37280e930a9dbf41dfa73d8d9
Jay Li <jayli3n@gmail.com>
2019-06-02 22:41:18 +0800

    authen works

---
+++ src/Components/Common/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+const Spinner = () => {
+	return (
+		<View>
+			
+		</View>
+	);
+};
+
+export { Spinner };
--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { Card, CardSection, Button, Input } from './Common';
+import firebase from 'firebase';
 
 
 class LoginForm extends Component {
@@ -7,11 +9,30 @@ class LoginForm extends Component {
 		super();
 		this.state = {
 			email: '',
-			password: ''
+			password: '',
+			error: ''
 		};
 	}
 
+	onButtonPress() {
+		const { email, password } = this.state;
+		this.setState({
+			error: ''
+		});
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.catch(() => {
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+				.catch(({ message }) => {
+					this.setState({
+						error: message
+					});
+				});
+			});
+	}
+
 	render() {
+		const { errorTextStyle } = styles;
+
 		return (
 			<Card>
 				<CardSection>
@@ -34,11 +55,25 @@ class LoginForm extends Component {
 				</CardSection>
 
 				<CardSection>
-					<Button text='Log In' />
+					<Button 
+						text='Log In'
+						onPress={this.onButtonPress.bind(this)}
+					/>
 				</CardSection>
+
+				<Text style={errorTextStyle}>
+					{this.state.error}
+				</Text>
 			</Card>
 		);
 	}
 }
+
+const styles = {
+	errorTextStyle: {
+		alignSelf: 'center',
+		color: 'red',
+	}
+};
 
 export default LoginForm;
