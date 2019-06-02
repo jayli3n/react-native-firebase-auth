111e2ad5ac752e620203ee21d6436fb657985761
Jay Li <jayli3n@gmail.com>
2019-06-03 00:08:51 +0800

    added auth flow

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 import LoginForm from './Components/LoginForm';
-import { Header } from './Components/Common';
+import { Header, CardSection, Button, Spinner } from './Components/Common';
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			loggedIn: null
+		};
+	}
+
 	componentWillMount() {
 		const firebaseConfig = {
 			apiKey: 'AIzaSyDrxVWflbyGP13HtgFgxXL-S02YI2EQTRs',
@@ -17,13 +24,62 @@ class App extends Component {
 		};
 		// Initialize Firebase
 		firebase.initializeApp(firebaseConfig);
+
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+				this.setState({
+					loggedIn: true
+				});
+			} else {
+				this.setState({
+					loggedIn: false
+				});
+			}
+		});
+	}
+
+	onLogoutButtonPress() {
+
+	}
+
+	renderContent() {
+		console.log(this.state.loggedIn);
+		if (this.state.loggedIn === true) {
+			return (
+				<View>
+					<Header headerText='Account' />
+					<CardSection>
+						<Button 
+							text='Log Out'
+							onPress={() => this.onLogoutButtonPress()}
+						/>
+					</CardSection>
+				</View>
+			);
+		} else if (this.state.loggedIn === false) {
+			return (
+				<View>
+					<Header headerText='Log In' />
+					<LoginForm />
+				</View>
+			);
+		} else {
+			return (
+				<Spinner size='large' />
+			);
+		}
 	}
 
 	render() {
 		return (
-			<View>
-				<Header headerText='Log In' />
-				<LoginForm />
+			<View style={{
+						position: 'absolute',
+						top: 0,
+						left: 0, 
+						right: 0,
+						bottom: 0,
+					}}>
+				{this.renderContent()}
 			</View>
 		);
 	}
